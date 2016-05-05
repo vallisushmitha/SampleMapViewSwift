1f16f377ef5dd372517f181dd0a0ff696eed2f90
kvanaMini1 <kvanamini1@kvanamini1s-mac-mini.local>
2016-05-05 16:44:10 +0530

    Initial Commit

---
+++ SampleMapView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleMapView
+//
+//  Created by kvanaMini1 on 05/05/16.
+//  Copyright © 2016 kvanaMini1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
